4d9be693d6d519bb4bd3938267ae4177a6e0407c
shubhangiisingh <singhshubhangi632@gmail.com>
2025-02-27 19:18:27 +0530

    middleware deleted

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-
-import { clerkMiddleware } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
-
-export default clerkMiddleware();
-
-export const config = {
-  matcher: "/((?!_next|static|favicon.ico).*)",
-};
