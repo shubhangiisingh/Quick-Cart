601ce1601b71a328514ba8883d74e848066de18e
shubhangiisingh <singhshubhangi632@gmail.com>
2025-04-19 19:19:27 +0530

    Second Commit

---
+++ config/db.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+let cached = global.mongoose
+if(!cached) {
+    cached = global.mongoose = {conn: null, promise: null}
+}
+
+const dbConnect = async function connectDB() {
+    
+
+    if(cached.conn) {
+        return cached.conn
+    }
+    if(!cached.promise) {
+        const opts = {
+            bufferCommands: false,
+        }
+        cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/Project0`, opts).then((mongoose) => {
+            return mongoose
+        })
+    }
+    try {
+        cached.conn = await cached.promise
+        return cached.conn
+    } catch (e) {
+        cached.promise = null
+        throw e
+        }
+}
