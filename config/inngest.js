28c221ffc75d45023c969db2dcd0b860cf9553f4
shubhangiisingh <singhshubhangi632@gmail.com>
2025-04-19 19:33:24 +0530

    Corrected

--- config/inngest.js
+++ config/inngest.js
@@ -1,4 +1,8 @@
+
 import { Inngest } from "inngest";
+import { connectDB } from "./db";
+import User from "../models/User";
+
 import { connect } from "mongoose";
 
 // Create a client to send and receive events
@@ -6,7 +10,7 @@ export const inngest = new Inngest({ id: "quickcart-next" });
 // Inngest function to save user data to a database
 export const syncUserCreation = inngest.function({
     id: "sync-user-from-clerk",
-},
+},  
 { event: "clerk/user.created" },
 async ({ event }) => {
     const{id, first_name, last_name, email_addresses, image_url} = event.data.user;
