58c4f1a7ad46cf7437e4a49780b2cea5861a5d40
shubhangiisingh <singhshubhangi632@gmail.com>
2025-04-19 20:08:37 +0530

    createFunction() updated

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,21 +1,10 @@
-// import { serve } from "inngest/next";
-// import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
-
-// // Create an API that serves zero functions
-// export const { GET, POST, PUT } = serve({
-    
-//   client: inngest,
-//   functions: [
-//     syncUserCreation,
-//     syncUserUpdation,
-//     syncUserDeletion
-//   ],
-// });
-// export const inngest = new Inngest({ name: "Quick Cart App" });
 import { serve } from "inngest/next";
+import { Inngest } from "inngest";
 import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
 
+// Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
+    
   client: inngest,
   functions: [
     syncUserCreation,
@@ -23,3 +12,4 @@ export const { GET, POST, PUT } = serve({
     syncUserDeletion
   ],
 });
+// export const inngest = new Inngest({ name: "Quick Cart App" });
--- config/inngest.js
+++ config/inngest.js
@@ -8,7 +8,7 @@ import { connect } from "mongoose";
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "quickcart-next" });
 // Inngest function to save user data to a database
-export const syncUserCreation = inngest.function({
+export const syncUserCreation = createFunction({
     id: "sync-user-from-clerk",
 },  
 { event: "clerk/user.created" },
@@ -25,7 +25,7 @@ async ({ event }) => {
 }
 )
 // Inngest Function to update user data in the database
-export const syncUserUpdation = inngest.createFunction({
+export const syncUserUpdation = createFunction({
     id: "update-user-from-clerk",
 },
 { event: "clerk/user.updated" },
@@ -42,7 +42,7 @@ async ({ event }) => {
 }
 )
 //Inngest function to delete user from database
-export const syncUserDeletion = inngest.function({
+export const syncUserDeletion = createFunction({
     id: "delete-user-with-clerk",
 },
 { event: "clerk/user.deleted" },
